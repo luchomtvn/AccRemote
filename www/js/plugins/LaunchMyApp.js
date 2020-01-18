b2c947b54b7d1876c7af25ec60e7fc3bf864078f
Luciano Mantovani <lucianomanto@gmail.com>
2020-01-18 08:00:52 -0300

    added custom url scheme plugin.

--- www/js/plugins/LaunchMyApp.js
+++ www/js/plugins/LaunchMyApp.js
@@ -1,9 +1,46 @@
-"use strict";
+(function () {
+    "use strict";
 
-/*
- Q: Why an empty file?
- A: iOS doesn't need plumbing to get the plugin to work, so..
-    - Including no file would mean the import in index.html would differ per platform.
-    - Also, using one version and adding a userAgent check for Android feels wrong.
-    - And if you're not using PhoneGap Build, you could paste your handleOpenUrl JS function here.
-*/
+  var remainingAttempts = 10;
+
+  function waitForAndCallHandlerFunction(url) {
+    if (typeof window.handleOpenURL === "function") {
+      // Clear the intent when we have a handler (note that this is only done when the preference 'CustomURLSchemePluginClearsAndroidIntent' is 'true' in config.xml
+      cordova.exec(
+          null,
+          null,
+          "LaunchMyApp",
+          "clearIntent",
+          []);
+
+      window.handleOpenURL(url);
+    } else if (remainingAttempts-- > 0) {
+      setTimeout(function(){waitForAndCallHandlerFunction(url);}, 500);
+    }
+  }
+
+  function triggerOpenURL() {
+    cordova.exec(
+        waitForAndCallHandlerFunction,
+        null,
+        "LaunchMyApp",
+        "checkIntent",
+        []);
+  }
+
+  document.addEventListener("deviceready", triggerOpenURL, false);
+
+  var launchmyapp = {
+    getLastIntent: function(success, failure) {
+      cordova.exec(
+        success,
+        failure,
+        "LaunchMyApp",
+        "getLastIntent",
+        []);
+    }
+  }
+
+  module.exports = launchmyapp;
+
+}());
